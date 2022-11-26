b82492906a830ef0157f7eeb00b68e6bfe12f79e
Diti Nitin Divekar <79137306+DITI2209@users.noreply.github.com>
2022-11-27 00:36:32 +0530

    Create app.dart

---
+++ lib/app.dart
@@ -0,0 +1,38 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pokego/configs/constants.dart';
+import 'package:pokego/configs/theme.dart';
+import 'package:pokego/routes.dart';
+import 'package:pokego/states/theme/theme_cubit.dart';
+
+class PokedexApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var themeCubit = BlocProvider.of<ThemeCubit>(context, listen: true);
+    var isDark = themeCubit.isDark;
+
+    return MaterialApp(
+      color: Colors.white,
+      title: 'PokeGo',
+      theme: isDark ? Themings.darkTheme : Themings.lightTheme,
+      navigatorKey: AppNavigator.navigatorKey,
+      onGenerateRoute: AppNavigator.onGenerateRoute,
+      builder: (context, child) {
+        if (child == null) return SizedBox.shrink();
+
+        final data = MediaQuery.of(context);
+        final smallestSize = min(data.size.width, data.size.height);
+        final textScaleFactor = min(smallestSize / AppConstants.designScreenSize.width, 1.0);
+
+        return MediaQuery(
+          data: data.copyWith(
+            textScaleFactor: textScaleFactor,
+          ),
+          child: child,
+        );
+      },
+    );
+  }
+}
